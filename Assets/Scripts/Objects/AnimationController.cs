fa40a7ba039782929e0e692de1078bb9486dacaf
Timur Khaimovich <itima.com@gmail.com>
2020-08-10 16:12:59 +0600

    Better Portal Visuals

---
+++ Assets/Scripts/Objects/AnimationController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] float minAnimationSpeed = .7f;
+    [SerializeField] float maxAnimationSpeed = 1.3f;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("SpinSpeed", Random.Range(minAnimationSpeed, maxAnimationSpeed));
+    }
+}
